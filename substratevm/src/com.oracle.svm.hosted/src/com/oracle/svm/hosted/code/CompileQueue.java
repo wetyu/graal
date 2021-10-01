1784220038e2a7efbf5e47a73ce0b16ba6ad7fd4
David Kozak <david.kozak@oracle.com>
2021-10-01 17:16:10 +0200

    implementation in progress

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompileQueue.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompileQueue.java
@@ -1023,6 +1023,7 @@ public class CompileQueue {
                 graph = new StructuredGraph.Builder(getCustomizedOptions(debug), debug).method(method).build();
             }
         }
+        method.compilationInfo.graph = graph;
         try (DebugContext.Scope s = debug.scope("Parsing", graph, method, this)) {
 
             try {
