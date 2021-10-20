fa1c234d23dae9211102494a6ffb6d0aac8578f1
David Kozak <david.kozak@oracle.com>
2021-10-20 15:16:16 +0200

    resolve style issues

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -390,7 +390,6 @@ suite = {
             "dependencies": [
                 "com.oracle.objectfile",
                 "com.oracle.svm.core",
-                "com.oracle.graal.pointsto",
                 "com.oracle.graal.reachability"
             ],
             "requiresConcealed": {
